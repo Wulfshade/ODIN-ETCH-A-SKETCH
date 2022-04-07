38a4821997bd66992e95661de4b5ebff1614701e
Ferdinando Brito <fantasyworldpt@gmail.com>
2022-04-07 11:02:47 +0100

    Style the board Generate a 16x16 grid of divs inside the board div

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let gridSize = 16;
+let pixelCount = gridSize * gridSize;
+let pixelSize = 960 / gridSize;
+
+function createPixel(pixelSize) {
+  let pixel = document.createElement("div");
+  pixel.style.height = `${pixelSize}px`;
+  pixel.style.width = `${pixelSize}px`;
+  pixel.style.backgroundColor = "black";
+
+  return pixel;
+}
+
+let board = document.querySelector("#board");
+
+for (let i = 0; i < pixelCount; i++) { 
+  pixel = createPixel(pixelSize);
+  board.appendChild(pixel);
+}
